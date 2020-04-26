5cd38eaaacabb8c6559c1925098816df3f014be2
Georges Chitiga <cgeorges89@gmail.com>
2020-04-26 23:00:01 +0200

    Add onSale advanced filter

--- src/components/Filters/onSaleFilter.js
+++ src/components/Filters/onSaleFilter.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 import { Form } from 'react-bootstrap';
 
 import { setFilters } from '../../actions/appActions';
-import { selectFilters, selectFilteredFilters } from '../../selectors/filters';
-import { selectFilteredProducts } from '../../selectors/products';
 
 const mapDispatchToProps = (dispatch) => {
     return {
@@ -13,24 +11,6 @@ const mapDispatchToProps = (dispatch) => {
         }
     };
 };
-const mapStateToProps = (state) => {
-    const filters = selectFilters(state);
-
-    // Retrieve all filters except the current one 
-    let filteredFilters = selectFilteredFilters(state, 'categoryFilter');
-
-    // Filter out products that had been already filtered by other filters
-    const filteredProducts = selectFilteredProducts(state, filteredFilters);
-    
-    // Retrieve categories from already filtered products
-    const categories = [...new Set(Object.keys(filteredProducts).map((product) => {
-        return filteredProducts[product].categories[0].name;
-    }))];
-    return {
-        categories,
-        filters,
-    }
-};
 
 class OnSaleFilter extends PureComponent {
     render() {
@@ -38,26 +18,26 @@ class OnSaleFilter extends PureComponent {
             <Form.Check
                 inline
                 label="Show only on sale"
+                onChange={this.handleSaveFilter.bind(this)}
                 type="checkbox"
                 id="advanced-on-sale"
             />
         );
     }
 
-    handleSaveFilter(selection) {
+    handleSaveFilter(event) {
         const { setFilters, filters } = this.props;
 
         let newFilters = {
             ...filters,
-            categoryFilter: {
-                'categories.name': selection,
+            onSaleFilter: {
+                'discounted': event.currentTarget.checked,
             }
         }
 
-        if (selection === 'clear') delete newFilters.categoryFilter;
         setFilters(newFilters);
 
     }
 }
 
-export default OnSaleFilter = connect(mapStateToProps, mapDispatchToProps)(OnSaleFilter);
+export default OnSaleFilter = connect(null, mapDispatchToProps)(OnSaleFilter);
